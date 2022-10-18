5b4f0592fc6cae5004f632174591494e3d6329a1
David Barath <davidbarath08@gmail.com>
2022-10-18 19:49:15 +0200

    Toggle effect with react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import { useState } from "react";
-import Title from "./components/Title"
+import React, { useState } from "react";
+import Title from "./components/Title";
+import Modal from './components/Modal';
 
 function App() {
+  const [showModal,setModal] = useState(false)
   const [showEvents, setShowEvents] =useState(true)
   const [events,setEvents] = useState([
     {title:"mario birthday bash", id: 1},
     {title:"mario birthday ", id: 2},
     {title:"mario ", id: 3}
   ]);
-  console.log(showEvents);
+  console.log(showModal);
 
   const handleclick =(id)=>{
     setEvents((prevEvents)=>{
@@ -20,12 +22,17 @@ function App() {
     console.log(id);
   }
 
+  const handleClose =() =>{
+    setModal(false)
+  }
+
   const subtitlestr = "All the latest events in MarioLand"
 
   return (
     <div className="App">
+      
       <Title Title="Events in your Area" subtitle={subtitlestr}/>
-      <Title Title="another title" subtitle="another"/>
+
       {showEvents && (
       <div>
           <button onClick={()=>setShowEvents(false)}>hide events</button>
@@ -39,11 +46,24 @@ function App() {
       )}
       
       {showEvents && events.map((event,i)=>  (
-        <div key={event.id}>
+        <React.Fragment key={event.id}>
           <h2> {event.title} - {i} </h2>
           <button onClick={()=> handleclick(event.id)}>delete event</button>
-        </div>
+        </React.Fragment>
       ))}
+
+      
+      <br />
+      {/* //modal nested */}
+      {showModal && <Modal handleClose={handleClose}>
+        <h2>Terms</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quia beatae iste, reiciendis ipsa distinctio quis expedita quibusdam sapiente pariatur.</p>
+      </Modal>}
+
+      <div>
+      <button onClick={()=>setModal(true)}>Show modal</button>
+      </div>
+
     </div>
   );
 }
